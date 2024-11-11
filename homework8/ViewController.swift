de04d3c946acc19ed6df5185dbdb4719d99caae5
huijuhnkim <huijuhnkim.dev@gmail.com>
2024-11-11 15:35:06 -0500

    Initial Commit

---
+++ homework8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework8
+//
+//  Created by Hui Juhn Kim on 11/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
